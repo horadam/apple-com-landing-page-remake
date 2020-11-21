f3a05d4ef6ef07706a38c245d1c94fc5d8bf64fb
Adam Horvath <adamhorvath108@gmail.com>
2020-11-21 15:33:44 -0800

    simplifying layout structure, improving small screen experience of MacPro div

--- src/components/MacPro.js
+++ src/components/MacPro.js
@@ -21,26 +21,24 @@ class MacPro extends Component {
                         transition: 'opacity 1000ms linear',
                     }}
                 >
-                    <div className="macProInnerWrapper">
                         <div className="macProImgWrapper">
                             <h2 className="headline">
                                 MacBook Pro 13"            
-                            </h2>   
+                            </h2>
+                            <h3 className="subhead"> 
+                                The M1&nbsp;chip comes to MacBook&nbsp;Pro. Bringing far more power. Incredible graphics. An&nbsp;advanced Neural&nbsp;Engine for enhanced machine learning. Superfast unified memory. And up to 20 hours of battery life — the longest‑ever battery life in&nbsp;a&nbsp;Mac.                </h3>
+                            <p className="availableStartingText">
+                                M1 chip models available starting 11.17
+                            </p>
+                            <div className="twoButtonsWrapper">
+                                <a href="https://apple.com" className="seriesButton">
+                                    Learn more
+                                </a>
+                                <a href="https://apple.com" className="seriesButton">
+                                    Buy
+                                </a>
+                            </div>
                         </div>
-                        <h3 className="subhead"> 
-                            The M1&nbsp;chip comes to MacBook&nbsp;Pro. Bringing far more power. Incredible graphics. An&nbsp;advanced Neural&nbsp;Engine for enhanced machine learning. Superfast unified memory. And up to 20 hours of battery life — the longest‑ever battery life in&nbsp;a&nbsp;Mac.                </h3>
-                        <p className="availableStartingText">
-                            M1 chip models available starting 11.17
-                        </p>
-                        <div className="twoButtonsWrapper">
-                            <a href="https://apple.com" className="seriesButton">
-                                Learn more
-                            </a>
-                            <a href="https://apple.com" className="seriesButton">
-                                Buy
-                            </a>
-                        </div>
-                    </div>
                 </div>
             </VizSensor>
         )
